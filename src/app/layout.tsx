2639a658695e840dacb8b16f97735d3f1d261676
jeongmin-dev <ljeongmin0908@gmail.com>
2024-06-24 21:13:56 +0900

    feat: 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto, Noto_Sans_KR } from "next/font/google";
 import { Providers } from "./Providers";
 
 import "./globals.css";
+import { cls } from "@/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSansKr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "400", "700", "900"],
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "400", "700"],
+  variable: "--roboto",
+});
 
 export const metadata: Metadata = {
   title: "서울시립미술관 소장품 정보",
@@ -17,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cls(notoSansKr.className, roboto.variable)}>
         <Providers>{children}</Providers>
       </body>
     </html>
