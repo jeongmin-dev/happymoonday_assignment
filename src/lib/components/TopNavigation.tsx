7f4005da098efd78b1a231f10a540d51144f951e
jeongmin-dev <ljeongmin0908@gmail.com>
2024-06-27 17:26:18 +0900

    feat: 상단 네비게이션 컴포넌트 추가

---
+++ src/lib/components/TopNavigation.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import { BackIcon } from "../styles/Icons";
+import { useRouter } from "next/navigation";
+
+interface TopNavigationProps {
+  title?: string;
+  rightIcon?: React.ReactNode;
+}
+export default function TopNavigation({
+  title = "",
+  rightIcon = "",
+}: TopNavigationProps) {
+  const router = useRouter();
+
+  const handleBackButton = () => {
+    router.back();
+  };
+
+  return (
+    <TopNavigationContainer>
+      <BackButtonContainer onClick={handleBackButton}>
+        <BackIcon />
+      </BackButtonContainer>
+      <h1>{title}</h1>
+      <RightIconContainer>{rightIcon}</RightIconContainer>
+    </TopNavigationContainer>
+  );
+}
+
+const TopNavigationContainer = styled.nav`
+  position: sticky;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  align-items: start;
+  justify-content: space-between;
+  padding: 1rem 0;
+  background-color: white;
+`;
+
+const BackButtonContainer = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  & svg {
+    color: #c9c9c9;
+    &:hover {
+      color: #999999;
+    }
+  }
+`;
+
+const RightIconContainer = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
