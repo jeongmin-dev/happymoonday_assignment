e792f4f66302dcf0075b94a4d93f0a43e240c51a
jeongmin-dev <ljeongmin0908@gmail.com>
2024-06-27 17:24:45 +0900

    feat: 환경설정 파일 추가 및 config 파일 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      { protocol: "http", hostname: "collections.eseoul.go.kr" },
+    ],
+  },
 };
 
 export default nextConfig;
