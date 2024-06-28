a821dceb4323cdf5ff4b7dea1314b924d51a9c01
jeongmin-dev <ljeongmin0908@gmail.com>
2024-06-28 15:11:46 +0900

    feat: api path 생성 함수 추가

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,18 @@
+import { GetItemsRequest } from "../services/apis/search.types";
+
 export const cls = (...classnames: string[]) => {
   return classnames.join(" ");
+};
+
+export const createPath = (request: GetItemsRequest) => {
+  const valueMap = Object.entries(request)
+    .map(([_, value]) => {
+      if (value === "") {
+        return "";
+      }
+      return value;
+    })
+    .join("/");
+
+  return valueMap;
 };
